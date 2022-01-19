783df3a9260a0a25190df875559e652cc99c0599
dhinesh.s <dhinesh.s@collabera.com>
2022-01-19 14:12:36 +0530

    docker file updation

--- src/main/java/com/jenkinsapp/jenkinscicd/JenkinsCiCdApplication.java
+++ src/main/java/com/jenkinsapp/jenkinscicd/JenkinsCiCdApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsCiCdApplication {
-
+//dhinesh
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsCiCdApplication.class, args);
 	}
